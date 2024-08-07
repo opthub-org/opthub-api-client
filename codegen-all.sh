eab78250c68fb68a84bb72d09e43907ccb57c7c7
Lane <lanexpr@outlook.jp>
2024-08-07 16:14:59 +0900

    :fix: bug in batch-generating script.

--- codegen-all.sh
+++ codegen-all.sh
@@ -5,8 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 for GEN_SCRIPT in $(ls $SCRIPT_DIR/*-codegen.sh); do
     FILENAME=$(basename $GEN_SCRIPT)
     LANG=${FILENAME%-codegen.sh}
-    shopt -s dotglob
-    yes | rm -rf $SCRIPT_DIR/opthub-api-client-$LANG/*
+    find $SCRIPT_DIR/opthub-api-client-$LANG -mindepth 1 -maxdepth 1 ! -name '.git' -exec rm -rf {} +
     $GEN_SCRIPT
     cp $SCRIPT_DIR/LICENSE $SCRIPT_DIR/opthub-api-client-$LANG/
 done
