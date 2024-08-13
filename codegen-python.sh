66fab517d50602bb87c94c335d7f34cf13c89cc9
Lane <lanexpr@outlook.jp>
2024-08-13 12:13:40 +0900

    :fix: opthub-org/opthub-api-client-python#1: a bug in the example code of the auto-generated documentation.

--- codegen-python.sh
+++ codegen-python.sh
@@ -10,3 +10,7 @@ docker run --rm -u $(id -u):$(id -g) -v $SCRIPT_DIR:/local openapitools/openapi-
         --git-user-id opthub-org \
         --git-repo-id opthub-api-client-python
 
+for PATCH_FILE in $(ls $SCRIPT_DIR/patch/python/*.patch); do
+    echo "Applying patch '$PATCH_FILE'..."
+    patch -p1 --directory=$SCRIPT_DIR/opthub-api-client-python < $PATCH_FILE
+done
