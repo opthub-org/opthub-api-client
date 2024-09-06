50a7e35ce84d9473c7a862ce7fd93da453676841
Lane <lanexpr@outlook.jp>
2024-09-06 15:43:50 +0900

    :fix: generated python package version.

--- codegen-python.sh
+++ codegen-python.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set -e
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+API_VERSION=$( docker run --rm -v $SCRIPT_DIR:/work mikefarah/yq '.info.version' /work/opthub-spec.yaml )
 
 docker run --rm -u $(id -u):$(id -g) -v $SCRIPT_DIR:/local openapitools/openapi-generator-cli \
     generate \
@@ -8,7 +11,8 @@ docker run --rm -u $(id -u):$(id -g) -v $SCRIPT_DIR:/local openapitools/openapi-
         --output /local/opthub-api-client-python \
         --package-name opthub_api_client \
         --git-user-id opthub-org \
-        --git-repo-id opthub-api-client-python
+        --git-repo-id opthub-api-client-python \
+        --additional-properties packageVersion=$API_VERSION
 
 for PATCH_FILE in $(ls $SCRIPT_DIR/patch/python/*.patch); do
     echo "Applying patch '$PATCH_FILE'..."
